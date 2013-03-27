f92766b878c024e8928df3b1d6b253a64c12fb80
Ruben Kostucki <rubenkostucki@gmail.com>
2013-03-27 11:28:54 +0000

    test balance decrease when bought

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,15 @@
 class Player
 
-	def lands
-		@lands = []
-	end
+  attr_accessor :lands, :balance
 
-	def buys(lands)
-		@lands << lands
+  def initialize
+    @lands = []
+    @balance = 1000
+  end
+
+	def buys(land)
+		@lands << land
+    @balance = @balance - land.value
   end
 
 end
--- test/test_player.rb
+++ test/test_player.rb
@@ -8,6 +8,7 @@ class TestPlayer < MiniTest::Unit::TestCase
 
 	def setup
 		@player1 = Player.new
+    @land1 = Land.new("Brussels", 900)
 	end
 
 	def test_no_lands_when_initialized
@@ -15,8 +16,17 @@ class TestPlayer < MiniTest::Unit::TestCase
 	end
 
 	def test_has_lands_when_bought
-    @player1.buys("Brussels")
+    @player1.buys(@land1)
 		assert_equal 1, @player1.lands.count
 	end
+
+  def test_balance_when_initialized
+    assert_equal 1000, @player1.balance
+  end
+
+  def test_balance_decrease_when_land_bought
+    @player1.buys(@land1)
+    assert_equal 100, @player1.balance
+  end
 
 end
